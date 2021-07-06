941c4256f9ef5fd1a74f794a385e14751ed7ef16
Alex <alexeytitovwork@gmail.com>
2021-07-06 19:42:00 +0300

    sources

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python3
+
 from restorator.video_restorator import *
 
 if __name__ == '__main__':
-    restrore_and_transcode()
+    # path_video_src = "resources/video_input.mp4"
+    # path_video_dst = "resources/video_output.mp4"
+
+    # path_video_src = "../../Git_repo_telesoft/DATA/test6_leave/master2_qa.mp4"
+    path_video_src = "../../Git_repo_telesoft/DATA/test6_leave/fish8_qa.mp4"
+
+    path_video_dst = "resources/fish8_qa_restored.mp4"
+
+    restrore_and_transcode(path_video_src, path_video_dst)
     #debug()
--- restorator/video_restorator.py
+++ restorator/video_restorator.py
@@ -1,15 +1,14 @@
+#!/usr/bin/env python3
 import cv2
 
-path_video_src = "../"
-path_video_dst = "../"
 
 
-def restrore_and_transcode(path_video_src, path_video_destanation):
+def restrore_and_transcode(path_video_src, path_video_dst):
     capture = cv2.VideoCapture(path_video_src)
     # use codec H264 to restore video time duration lag in file.
     # https: // www.programcreek.com / python / example / 89348 / cv2.VideoWriter_fourcc
     fourCodec = cv2.VideoWriter_fourcc(*'H264')
-    out = cv2.VideoWriter(path_video_dst, fourCodec, 20.0, (1024, 512))
+    out = cv2.VideoWriter(path_video_dst, fourCodec, 10.0, (1024, 512))
     while (capture.isOpened()):
         ret, frame = capture.read()
         if ret == True:
