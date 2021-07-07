2253d1a50c423c4467df7b9b320d81263badff19
AlexT <alexeytitovwork@gmail.com>
2021-07-07 15:33:12 +0300

    Update writer with avi format.

--- restorator/video_decimator.py
+++ restorator/video_decimator.py
@@ -1,7 +1,11 @@
+
 import cv2
 
 # path_video_src = "/home/interceptor/Документы/Git_repo_telesoft/DATA/videot_3/fish8_qa.mp4"
 path_video_src = "/home/interceptor/Документы/DATA/test5/fish4_1min.mp4"
+
+# Write only in AVI format
+path_video_dst = "/home/interceptor/Документы/DATA/test5/output.avi"
 
 
 cap = cv2.VideoCapture(path_video_src)
@@ -11,10 +15,15 @@ fps = cap.get(cv2.CAP_PROP_FPS)
 
 # Get the total numer of frames in the video.
 frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-
 frame_number = 0
 cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)  # optional
 success, frame = cap.read()
+# Writer
+# fourCodec = cv2.VideoWriter_fourcc('h', '2', '6', '4')
+# out = cv2.VideoWriter(path_video_dst, fourCodec, 10, (1280, 1280))
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter(path_video_dst, fourcc, 10.0, (1280,  1280))
 
 while success and frame_number <= frame_count:
     # do stuff
@@ -22,8 +31,14 @@ while success and frame_number <= frame_count:
     frame_number += fps
     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
     success, frame = cap.read()
-    cv2.imshow("Video", frame)
+
+    if success:
+        cv2.imshow("Video", frame)
+        out.write(frame)
+    else:
+        break
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
+out.release()
--- restorator/video_restorator.py
+++ restorator/video_restorator.py
@@ -2,16 +2,20 @@
 import cv2
 
 # path_video_src = "../resources/fish8_leave.mp4"
-path_video_src = "/home/interceptor/Документы/Git_repo_telesoft/DATA/videot_3/fish8_qa.mp4"
+# path_video_src = "/home/interceptor/Документы/Git_repo_telesoft/DATA/videot_3/fish8_qa.mp4"
+path_video_src = "/home/interceptor/Документы/DATA/test5/fish4_1min.mp4"
 
 # path_video_dst = "../resources/fish8_qa_restored.mp4"
-path_video_dst = "/home/interceptor/Документы/Git_repo_telesoft/DATA/videot_3/fish8_tahoe.mp4"
+# path_video_dst = "/home/interceptor/Документы/Git_repo_telesoft/DATA/videot_3/fish8_tahoe.mp4"
+path_video_dst = "/home/interceptor/Документы/DATA/test5/fish4_1min_restoration.avi"
 
 def restrore_and_transcode(path_video_src, path_video_dst):
     capture = cv2.VideoCapture(path_video_src)
     # use codec H264 to restore video time duration lag in file.
     # https: // www.programcreek.com / python / example / 89348 / cv2.VideoWriter_fourcc
     fourCodec = cv2.VideoWriter_fourcc(*'H264')
+    # fourCodec = cv2.VideoWriter_fourcc(*'avc1')
+    # fourCodec = cv2.VideoWriter_fourcc('h', '2', '6', '4')
     # out = cv2.VideoWriter(path_video_dst, fourCodec, 120.0, (1280, 1280))
     # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(path_video_dst, fourCodec, 10.0, (1280, 1280))
@@ -19,15 +23,14 @@ def restrore_and_transcode(path_video_src, path_video_dst):
     while (capture.isOpened()):
         ret, frame = capture.read()
         if ret == True:
+            # print("Frame write:" + str(ret))
+            cv2.imshow('frame', frame)
             out.write(frame)
-            print("Frame write:" + str(ret))
-            cv2.imwrite('../resources/image.png', frame)
-            # cv2.imshow('frame', frame)
-            # choice = input("\033[1;32;40m Press 'Q' and then 'Enter' stop.\033[0m \n")
-            # if choice == "Q" or choice == "q":
-            #     break
         else:
             break  # Release all if complete
+
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
     capture.release()
     out.release()
     # cv2.destroyAllWindows()
